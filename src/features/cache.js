555840db033d074b0de5622822d56f655ecfa9cf
Gabin Desserprit <desserprit.gabin@gmail.com>
2020-07-31 10:18:14 +0200

    Fix cache feature

--- src/features/cache.js
+++ src/features/cache.js
@@ -14,15 +14,13 @@ export default {
     if (localStorage) {
       // Fetched saved state when exists
       const cached = localStorage.getItem(prefix(space, name))
-      if (cached) store.state = { ...store.state, ...JSON.parse(cached) }
+      if (cached) store.state = JSON.parse(cached)
       // Watch for mutations, save them
       setWatchers(
         { space, name },
         {
-          handler(newState, oldState) {
-            if (newState !== oldState) {
-              localStorage.setItem(prefix(space, name), JSON.stringify(newState))
-            }
+          handler(newState) {
+            localStorage.setItem(prefix(space, name), JSON.stringify(newState))
           },
           immediate: true,
           deep: true,
